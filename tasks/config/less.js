874194352159545c43b60cb345406f7fc5e19da9
sisense-dev <liran.shani@sisense.com>
2015-06-24 12:42:28 +0300

    update

---
+++ tasks/config/less.js
@@ -0,0 +1,27 @@
+module.exports = {
+  options: {
+    sourceMapBasepath: '<%= src %>/kibana',
+    sourceMapRootpath: '/',
+    sourceMapFileInline: true,
+    ieCompat: false,
+    paths: [
+      '<%= src %>/kibana/bower_components/lesshat/build/'
+    ]
+  },
+  dev: {
+    src: '<%= lessFiles %>',
+    expand: true,
+    ext: '.css',
+    options: {
+      sourceMap: true
+    }
+  },
+  build: {
+    src: '<%= lessFiles %>',
+    expand: true,
+    ext: '.css',
+    options: {
+      sourceMap: false
+    }
+  }
+};
